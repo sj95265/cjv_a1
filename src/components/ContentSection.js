346a77c77004fe8f602d6ad056684be96cd9bda8
sj95265 <sj95265@gmail.com>
2024-07-04 07:08:24 -0400

    Content Section

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Box, Heading, Text, Image } from '@chakra-ui/react';
 
-const ContentSection = () => (
-    <section>
-        <h2>About Us</h2>
-        <p>Welcome to the Digital Video Store. We provide the best movies and TV shows from around the world.</p>
-        {/* Add more static content as needed */}
-    </section>
-);
+const ContentSection = () => {
+  return (
+    <Box p={5} borderRadius="md" boxShadow="md" mb={8} >
+      <Heading as="h2" size="lg" mb={4} textAlign="center">About Us</Heading>
+      <Text fontSize="lg" mb={4} textAlign="center">
+        DigiVideo is a movie and TV show streaming site that you can buy or rent, which allows you to watch high quality movies or TV shows online.
+      </Text>
+      {/* <Box display="flex" justifyContent="center">
+        <Image src="https://via.placeholder.com/600x400" alt="Streaming" borderRadius="md" />
+      </Box> */}
+    </Box>
+  );
+};
 
 export default ContentSection;
