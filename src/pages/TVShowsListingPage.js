ee84f1d90dccf1de5d55e2a264e549a2996823f9
sj95265 <30563417+sj95265@users.noreply.github.com>
2024-07-05 08:44:58 -0400

    Update TVShowsListingPage.js

--- src/pages/TVShowsListingPage.js
+++ src/pages/TVShowsListingPage.js
@@ -5,7 +5,7 @@ const TVShowsListingPage = () => {
   const [tvShows, setTVShows] = useState([]);
 
   useEffect(() => {
-    fetch('https://json-server-vercel-6dx0dcw1z-leos-projects-d449feca.vercel.app/tvShows')
+    fetch('https://json-server-vercel-mu-sable.vercel.app/tvShows')
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
